d3743fb51744b1427a2ef969a62b5ea19347a9b3
devsoft112 <devsoftswe.0205@gmail.com>
2024-01-12 21:15:37 -0500

    api implemented and cards rendering info

--- website/listings/listings.component.js
+++ website/listings/listings.component.js
@@ -2,5 +2,31 @@ angular
     .module('listings')
     .component('listings', {
         templateUrl: 'website/listings/listings.template.html',
-       
+        controller: ['$http', function propertiesController($http) {
+
+            var self = this;
+            self.listings = [];
+            var config = {
+                url: 'https://sandbox.repliers.io/listings?listings=true&operator=AND&sortBy=updatedOnDesc&status=A',  // Replace with the actual API endpoint URL
+                method: 'GET',  // Or any other HTTP method (POST, PUT, DELETE, etc.)
+                headers: {
+                  'REPLIERS-API-KEY': 'VMEs2eM1aBgCrM8tR9rG54Ve8hyRqo',  // Replace with your actual API key
+                  'Content-Type': 'application/json'
+                }
+              };
+
+           window.onload = function() {
+                $http(config)
+                .then(function successCallback(response) {
+                  // this callback will be called asynchronously
+                  // when the response is available
+                  console.log(response);
+                    self.listings = response.data.listings;
+                }, function errorCallback(response) {
+                  // called asynchronously if an error occurs
+                  // or server returns response with an error status.
+                });
+            }
+            
+        }]
     });
